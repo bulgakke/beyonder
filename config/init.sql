b1d56a8d28019d550f0e2b7f18f855516dce4c27
bulgakke <be.evgeniy.bulavin@gmail.com>
2025-08-11 15:35:30 +0400

    deploy: basic setup

---
+++ config/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS `beyonder_production`;
+CREATE DATABASE IF NOT EXISTS `beyonder_production_cache`;
+CREATE DATABASE IF NOT EXISTS `beyonder_production_queue`;
+CREATE DATABASE IF NOT EXISTS `beyonder_production_cable`;
