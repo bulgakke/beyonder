2f063d71fe66f613e739a9c1c428a1e7fe9e455b
Eugene <be.evgeniy.bulavin@gmail.com>
2025-08-18 20:13:11 +0400

    style: rearrange routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  resource :session
-  resources :passwords, param: :token
-
-  get "users/:username", to: "users#show", as: :user
-  resources :users, only: %i[create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,6 +8,11 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  resource :session
+  resources :passwords, param: :token
+
+  get "users/:username", to: "users#show", as: :user
+  resources :users, only: %i[create]
 
   # Defines the root path route ("/")
   root "sessions#new"
