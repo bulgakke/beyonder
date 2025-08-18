e6bb1e2bb875205ff84a6e97e0f61ee61443e4d9
Eugene <be.evgeniy.bulavin@gmail.com>
2025-08-18 19:55:15 +0400

    fix: remove init.sql

--- config/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `beyonder_production`;
-CREATE DATABASE IF NOT EXISTS `beyonder_production_cache`;
-CREATE DATABASE IF NOT EXISTS `beyonder_production_queue`;
-CREATE DATABASE IF NOT EXISTS `beyonder_production_cable`;
