202a2002f9d8123047d3da7aa71afcdc0c2396da
Eugene <be.evgeniy.bulavin@gmail.com>
2025-08-21 04:32:43 +0400

    fix: showing user avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def show_image(attachment, variant, **options)
+    if attachment.present?
+      image_tag(url_for(attachment.variant(variant)), **options)
+    else
+      image_tag("placeholder.svg", **options)
+    end
+  end
 end
