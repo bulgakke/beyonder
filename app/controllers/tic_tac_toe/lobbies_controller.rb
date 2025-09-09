041eb9a996e6dad31b3e2a7d1af2f9a693de2784
Eugene <be.evgeniy.bulavin@gmail.com>
2025-09-09 20:04:07 +0400

    feat: join/leave lobby

--- app/controllers/tic_tac_toe/lobbies_controller.rb
+++ app/controllers/tic_tac_toe/lobbies_controller.rb
@@ -16,9 +16,23 @@ module TicTacToe
     end
 
     def join
+      unless @lobby.users.include?(Current.user)
+        @lobby.lobby_participations.create!(user: Current.user)
+      end
+
+      respond_to do |format|
+        format.turbo_stream { render "lobbies/update", locals: { lobby: @lobby } }
+        format.html { redirect_to tic_tac_toe_lobby_path(@lobby) }
+      end
     end
 
     def leave
+      @lobby.lobby_participations.where(user: Current.user).destroy_all
+
+      respond_to do |format|
+        format.turbo_stream { render "lobbies/update", locals: { lobby: @lobby } }
+        format.html { redirect_to tic_tac_toe_lobby_path(@lobby) }
+      end
     end
 
     def start
